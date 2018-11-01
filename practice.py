f8a672b3db51f89f87e70515e1c9909de48f470f
yohanesam <34635051+yohanesam@users.noreply.github.com>
2018-11-01 09:55:20 +0700

    automaticaly create probe worker

--- practice.py
+++ practice.py
@@ -1,11 +1,25 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
+from sc2.constants import NEXUS, PROBE, PYLON
 from sc2.player import Bot, Computer
 
 class RectagonBot(sc2.BotAI) : 
     async def on_step (self, iteration) : 
         # what to do every step
         await self.distribute_workers() # in sc2/bot_ai.py
+        await self.build_workers()  #our own bot for worker
+
+    async def build_workers(self) :
+        # nexus is command center in sc2
+        # probe is worker in sc2
+        
+        # check if nexus is already build and does not create
+        # anything.
+        for nexus in self.units(NEXUS).ready.noqueue :
+            # And then check if there is enough recource to create probe
+            if self.can_afford(PROBE) :
+                # create the probe
+                await self.do(nexus.train(PROBE))
 
 run_game(
     maps.get("PaladinoTerminalLE"),
