1bd93dd557fcac80631494f80daaf3e0e599a464
yohanesam <34635051+yohanesam@users.noreply.github.com>
2018-11-02 13:36:06 +0700

    add gas-getter code

--- practice.py
+++ practice.py
@@ -1,6 +1,6 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
-from sc2.constants import NEXUS, PROBE, PYLON
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 from sc2.player import Bot, Computer
 
 class RectagonBot(sc2.BotAI) : 
@@ -9,7 +9,8 @@ class RectagonBot(sc2.BotAI) :
         await self.distribute_workers() # in sc2/bot_ai.py
         await self.build_workers()  # our own bot for worker
         await self.build_pylons() # build supply
-        await self.expand()
+        await self.expand() # expand base
+        await self.build_assimilators() # build the gas-getter building
         
 
     async def build_workers(self) :
@@ -37,6 +38,21 @@ class RectagonBot(sc2.BotAI) :
     async def expand(self) :
         if self.units(NEXUS).amount < 2 and self.can_afford(NEXUS) :
             await self.expand_now()
+
+    async def build_assimilators(self) :
+        for nexus in self.units(NEXUS).ready :
+            vespenes = self.state.vespene_geyser.closer_than(25.0, nexus)
+
+            for vespene in vespenes :
+                if not self.can_afford(ASSIMILATOR) :
+                    break
+                
+                worker = self.select_build_worker(vespene.position)
+                if worker is None :
+                    break
+                
+                if not self.units(ASSIMILATOR).closer_than(1.0, vespene).exists :
+                    await self.do(worker.build(ASSIMILATOR, vespene))
 
 run_game(
     maps.get("PaladinoTerminalLE"),
